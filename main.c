8b09512cf6c5f09cd6af0cdee7430b67c6a55098
Felipe Avila Silva <fasilva@inf.ufpel.edu.br>
2019-04-10 12:43:13 -0300

    codigo organizado-falta remover

--- main.c
+++ main.c
@@ -9,105 +9,71 @@ struct pessoa{
 };
 
 int main(){
-    void *pBuffer; //aloc
-    struct pessoa *p; //aloc
+    void *pBuffer;                  //somente ponteiros
+    struct pessoa *p;
     int *a, *cp, *i, *k;
-    char *trashnome; //alloc
-    cp = 0;
+    char *trashnome;
 
+    pBuffer = malloc((sizeof(int)*4));
 
-        /*if(*cp == 0){ // para nao zerar o malloc
-            cp = 1;
-        }*/
-    pBuffer = malloc((sizeof(int)*4) + (sizeof(struct pessoa)*5) + (sizeof(char)*20));
-        /*if(*cp == 1){ // recuperando valor original
-            cp = 0;
-        }*/
-            a = (int*)pBuffer;
-            cp = a + 1;
-            i = cp + 1;
-            k = i + 1;
-            trashnome = (char*)(k + 1);
-            p = (struct pessoa*)(trashnome + 1);
+        a = (int*)pBuffer;
+        cp = a + 1;
+        i = cp + 1;
+        k = i + 1;
+        trashnome = (char*)(k + 1);
+        p = (struct pessoa*)(trashnome + 1);
 
 do{
     printf("Escolha:\n\n 1 - incluir\n 2 - buscar\n 3 - listar\n 4 - remove\n 5 - sair\n\n"); scanf("%d", a);
 
-
-    if (*a >= 1 && *a<=5){
+    if(*a >= 1 && *a<=5){
         switch(*a){
             case 1:
-                *cp = *cp + 1;
-                pBuffer = realloc(pBuffer, (sizeof(int)*4) + (sizeof(struct pessoa)*(*cp)) + (sizeof(char)*20));
+                *cp = *cp + 1;                                      //contador de pessoa comeca com 1
+                pBuffer = realloc(pBuffer, (sizeof(int)*4) + (sizeof(struct pessoa)*(*cp)) + (sizeof(char)*20));       //realloc de 4 inteiros, struct de n pessoas, e 20 espaços para char
 
-                a = (int*)pBuffer;
-                cp = a + 1;
-                i = cp + 1;
-                k = i + 1;
-                trashnome = (char*)(k + 1);
-                p = (struct pessoa*)(trashnome + 1); //
+                    a = (int*)pBuffer;                              //set no endereço de memoria    //casting   //armazenamento de variaveis
+                    cp = a + 1;
+                    i = cp + 1;
+                    k = i + 1;
+                    trashnome = (char*)(k + 1);
+                    p = (struct pessoa*)(trashnome + 1);            //inicio da agenda
 
-                p = p + *cp;
-
-                printf("Nome:\n"); scanf("%s", p->nome);
-                printf("Idade:\n"); scanf("%d", &p->idade);
-
-                //*cp = *cp + 1;
-                //p = p + *cp;
-               // p = p + 1;
-
-
+                    p = p + *cp;                                    //pula pra proxima pessoa de acordo com o numero de pessoas
+                    printf("Nome:\n"); scanf("%s", p->nome);
+                    printf("Idade:\n"); scanf("%d", &p->idade);
             break;
 
             case 2:
                 printf("Digite o nome da pessoa que voce deseja buscar:\n");
-                scanf("%s", trashnome);
-
-                //printf("trash: %s\n", trashnome);
-                //printf("p->nome %s\n", p->nome);
-
+                scanf("%s", trashnome);                             //char para comparar o nome
 
                 if(*cp >= 1){
-                    p = (struct pessoa*)(trashnome + 1);
-                    p = p + 1;
+                    p = (struct pessoa*)(trashnome + 1);            //set comeco da agenda
+                    p = p + 1;                                      // pular 1, se nao buga na hora de buscar
                     for(*k = 0; *k < *cp; *k = *k + 1){
-                        //printf("p->nome %s\n", p->nome);
                            if(strcmp(p->nome, trashnome) == 0){
                                 printf("Nome: %s\n", p->nome);
                                 printf("Idade: %d\n\n", p->idade);
                             }
-                            p = p + 1;
+                            p = p + 1;                              //prox nome da agenda
                     }
                 }else{
                 printf("nao tem nenhuma pessoa na agenda:\n");
                 }
-
-
-
-
-
-
-           //   printf("i: %d, cp: %d", *i, *cp); //teste
-
             break;
 
             case 3:
-
                 if (*cp >= 1){
-                         p = (struct pessoa*)(trashnome + 1);
-
+                    p = (struct pessoa*)(trashnome + 1);    //set começo da agenda
                     for(*i = 0; *i < *cp; *i = *i + 1){
-                            p = p + 1;
-                        printf("Nome: %s\n", p->nome);
-                        printf("Idade: %d\n\n", p->idade);
-                       // p = p + 1;
-
+                            p = p + 1;                      //prox pessoa
+                            printf("Nome: %s\n", p->nome);
+                            printf("Idade: %d\n\n", p->idade);
                     }
                 }else{
                     printf("Nenhum usuario foi digitado:\n");
                 }
-
-
             break;
 
             case 4:
@@ -123,6 +89,5 @@ do{
         exit(1);
     }
    }while(*a != 5);
-
     return 0;
 }
